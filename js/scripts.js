f5188ab8d701e9b4fa5c20780c07a5db1cb42969
Chris LeCompte <student@epicodus.com>
2015-05-29 09:28:54 -0700

    add website functionality to word replacing app

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,18 @@ var wordReplacer = function(sentence, oldWord, newWord) {
 
   return finalArray.join(" ");
 }
+
+$(function() {
+
+  $("form#sentence").submit(function(event) {
+    var sentence = $("input#sentence").val();
+    var oldWord = $("input#oldWord").val();
+    var newWord = $("input#newWord").val();
+
+    var result = wordReplacer(sentence, oldWord, newWord);
+
+    $(".newSentence").text(result);
+
+    event.preventDefault();
+  })
+})
